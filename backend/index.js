bfb08de49e7fed3c9d45b4b6051871e5dae30a73
Shivam Dubey <118355740+shivam969619@users.noreply.github.com>
2024-09-07 18:00:52 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -29,7 +29,6 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.options("*", cors(corsOptions));  // Preflight requests for all routes
  // Allow preflight requests for all routes
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
