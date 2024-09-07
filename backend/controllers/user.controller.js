5236fa46ca93af7bd1e8bed63e96658a43835d9d
Shivam Dubey <118355740+shivam969619@users.noreply.github.com>
2024-09-07 20:57:52 +0530

    Update user.controller.js

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -95,7 +95,7 @@ export const login = async (req, res) => {
        .cookie("token", token, {
         maxAge: 1 * 24 * 60 * 60 * 1000,
         httpsOnly: true,
-        sameSite: "strict",
+        sameSite: "None",
       })
       .json({
         message: `Welcome to Jobhunt ${user.fullname}`,
